f4b0f551fdd873061eae94b970ec1bb7c56f0729
Catalin-code <catalin.despa.code@gmail.com>
2021-07-15 20:27:33 +0300

    Perform code cleanup on all files

--- Controllers/PrescriptionsController.cs
+++ Controllers/PrescriptionsController.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CodeMedical.Data;
+using CodeMedical.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using CodeMedical.Data;
-using CodeMedical.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CodeMedical.Controllers
 {
